e24caefce5f577d901dcab18185fa97f894bbfad
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2024-09-12 09:54:25 -0600

    minor

--- src/components/images/SvgRightMotion.tsx
+++ src/components/images/SvgRightMotion.tsx
@@ -6,8 +6,8 @@ const SvgRightMotion = () => {
   return (
     <>
       <motion.div
-        initial={{ x: 50, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
         className="w-[55%] h-[55%]  bg-secondary-opacity absolute -bottom-5 -right-5 z-[1]"
       ></motion.div>
